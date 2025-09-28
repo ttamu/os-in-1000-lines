8e388b1b778da416a306f1f67b6729b4d915359f
tamura <tomoki0927.7939@icloud.com>
2025-09-29 07:01:49 +0900

    Add simple page allocator

--- common.h
+++ common.h
@@ -11,6 +11,7 @@ typedef uint32_t vaddr_t;
 
 #define true  1
 #define false 0
+#define PAGE_SIZE 4096
 #define NULL  ((void *) 0)
 #define align_up(value, align)   __builtin_align_up(value, align)
 #define is_aligned(value, align) __builtin_is_aligned(value, align)
